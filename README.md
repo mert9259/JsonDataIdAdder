334a86f4f206c783785f5205a73f986ee23943b8
MERT <mert9259@gmail.com>
2020-04-26 17:54:43 +0300

    version1
    last version

---
+++ README.md
@@ -0,0 +1,7 @@
+How to use?
+
+1) You need to write uniqe key where we want to make changes in Data.js.
+
+2) You can do this by using ctrl + f keys with an editor such as notepad ++, and you can make the necessary editing from the change window in the corresponding window.
+
+3) When you run the program, your new json data will be saved to the data2.js file.
---
+++ jsonparser.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <conio.h>
+#include <stdlib.h>
+#include <fstream>
+
+using namespace std;
+
+
+int main() {
+
+	string line;
+	
+	ofstream fileW("data2.js");
+	ifstream fileR("data.js");
+	
+	int sayac=0;
+	
+	
+	while(fileR>>line){
+		if(line=="change"){
+			fileW<<"\"id\""<<": "<<sayac<<",\n";
+			sayac++;
+		}
+		else if(line!="{" || line!="},")
+			fileW<<line<<"\n";
+		else
+			fileW<<line<<"\n";
+	}
+	
+	
+	fileR.close();
+	fileW.close();
+	
+	return 0;
+}
+
